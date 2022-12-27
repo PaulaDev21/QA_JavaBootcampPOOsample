68bc82def8db0c809dec217362b8475b6001b267
Paula Lopes <pauladev21@gmail.com>
2022-12-27 20:50:31 -0300

    Adds classes Course and Mentorship in package br.com.pauladev.dio.bootcamp.gtf.qa.poobootcamp.domain

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import br.com.pauladev.dio.bootcamp.gtf.qa.poobootcamp.domain.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Course c = new Course();
+        Mentorship m = new Mentorship();
+
+        System.out.print(c);
+        System.out.print(m);
     }
 }
